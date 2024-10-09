65e67e82ce72535f1cf6142b5bc149087da4e843
lotrik84 <lotrikys@gmail.com>
2024-10-09 13:28:08 +0300

    * Add pagination support

--- books/spiders/books_toscrape.py
+++ books/spiders/books_toscrape.py
@@ -1,4 +1,7 @@
+from pathlib import Path
+
 import scrapy
+from scrapy.http import Response
 
 
 class BooksToscrapeSpider(scrapy.Spider):
@@ -6,5 +9,10 @@ class BooksToscrapeSpider(scrapy.Spider):
     allowed_domains = ["books.toscrape.com"]
     start_urls = ["https://books.toscrape.com/"]
 
-    def parse(self, response):
-        pass
+    def parse(self, response: Response, **kwargs):
+        for book in response.css("h3 > a::attr(href)").getall():
+            yield {"link": book}
+
+            next_page = response.css("li.next > a::attr(href)").get()
+            if next_page:
+                yield response.follow(next_page, callback=self.parse)
