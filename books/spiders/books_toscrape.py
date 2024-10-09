47fb4aa781056a202e3aaa3534b55eb9ce469530
lotrik84 <lotrikys@gmail.com>
2024-10-09 14:39:27 +0300

    * Add parse all books

--- books/spiders/books_toscrape.py
+++ books/spiders/books_toscrape.py
@@ -1,7 +1,8 @@
-from pathlib import Path
-
 import scrapy
 from scrapy.http import Response
+
+
+STARS = {"One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
 
 
 class BooksToscrapeSpider(scrapy.Spider):
@@ -11,8 +12,34 @@ class BooksToscrapeSpider(scrapy.Spider):
 
     def parse(self, response: Response, **kwargs):
         for book in response.css("h3 > a::attr(href)").getall():
-            yield {"link": book}
+            yield response.follow(book, callback=self._parse_single_book)
 
-            next_page = response.css("li.next > a::attr(href)").get()
-            if next_page:
-                yield response.follow(next_page, callback=self.parse)
+        next_page = response.css("li.next > a::attr(href)").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
+    @staticmethod
+    def _parse_single_book(response: Response):
+        yield {
+            "title": response.css("h1::text").get(),
+            "price": float(
+                response.css("p.price_color::text").get().replace("£", "")
+            ),
+            "amount_in_stock": int(
+                response.css("p.instock::text")
+                .getall()[1]
+                .split()[2]
+                .replace("(", "")
+            ),
+            "rating": STARS.get(
+                response.css(".product_main .star-rating")
+                .xpath("@class")
+                .extract()[0]
+                .split()[-1]
+            ),
+            "category": response.css(".breadcrumb li > a::text").getall()[-1],
+            "description": response.css(
+                "article.product_page > p::text"
+            ).get(),
+            "upc": response.css("table td::text").get(),
+        }
